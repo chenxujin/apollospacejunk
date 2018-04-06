eeac08717d6a5f0bf299007ae253b1a7e3793738
Charles Reid <charlesreid1@gmail.com>
2018-04-06 01:44:52 -0700

    Apollo bot, back in service

--- bot/ApolloBotFlock.py
+++ bot/ApolloBotFlock.py
@@ -1,21 +1,43 @@
 import rainbowmindmachine as rmm
-import logging
+import os, glob
+
+DATADIR = os.path.join(os.getcwd(), 'data')
+KEYSDIR = os.path.join(os.getcwd(), 'keys')
+LIVE = False
 
 def setup():
     k = rmm.TxtKeymaker()
-    k.make_keys('/home/charles/codes/apollospacejunk/bot/data/','/home/charles/codes/apollospacejunk/bot/keys/')
+    k.make_keys(DATADIR, KEYSDIR)
     
 def run():
-    sh = rmm.Shepherd('/home/charles/codes/apollospacejunk/bot/keys/',sheep_class=rmm.QueneauSheep)
+    sh = rmm.Shepherd(KEYSDIR, 
+                      flock_name = 'apollo',
+                      sheep_class=rmm.QueneauSheep)
 
-    sh.perform_pool_action('tweet',{
-            'publish' : False,
-            'inner_sleep' : 3,#3*60,
-            'outer_sleep' : 3,#2*3600,
-            'lines_length' : 4
-        })
+    if not LIVE:
+        sh.perform_pool_action('tweet',{
+                'publish' : False,
+                'inner_sleep' : 3,#3*60,
+                'outer_sleep' : 3,#2*3600,
+                'lines_length' : 4
+            })
+    else:
+        sh.perform_pool_action('tweet',{
+                'publish' : True,
+                'inner_sleep' : 3*60,
+                'outer_sleep' : 2*3600,
+                'lines_length' : 4
+            })
 
 
 if __name__=="__main__":
-    #setup()
-    run()
+
+    keys_exists = os.path.isdir(KEYSDIR)
+    keys_has_keys = len(glob.glob(os.path.join(KEYSDIR,"*.json"))) > 0
+    if( keys_exists and keys_has_keys ):
+        print("running bot")
+        run()
+    else:
+        print("setting up bot")
+        setup()
+
