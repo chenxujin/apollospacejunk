c68049bbdf0843a7684d611967cd253d8b95123b
Charles Reid <charlesreid1@gmail.com>
2018-04-06 00:24:02 -0700

    removing attic

--- attic/bot/Apollo11Ipsum.py
+++
@@ -1,7 +0,0 @@
-from Babble import generate_ipsum
-
-ipsum_file = "apollo11_ipsum.txt"
-data_file = "data/apollo_11.txt"
-
-generate_ipsum(ipsum_file,data_file,N=150)
-
--- attic/bot/Apollo12Ipsum.py
+++
@@ -1,6 +0,0 @@
-from Babble import generate_ipsum
-
-ipsum_file = "apollo12_ipsum.txt"
-data_file = "data/apollo_12.txt"
-
-generate_ipsum(ipsum_file,data_file,N=150)
